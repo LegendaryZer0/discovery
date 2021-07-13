0231fbb6ca942f5a9a7f6a1e8bdac38ad1951105
Rifdar <assasinnyx@mail.ru>
2021-07-13 20:56:58 +0300

    added spring-native with graalVM

--- src/main/java/ru/internship/cloud/discovery/EurekaServerApplication.java
+++ src/main/java/ru/internship/cloud/discovery/EurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
