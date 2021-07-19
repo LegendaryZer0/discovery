ed2e7f30a5a3fc96ef57d8dc229d0302c15392db
Rifdar <assasinnyx@mail.ru>
2021-07-19 21:17:01 +0300

    Minority changes, commented spring native plugin

--- src/main/java/ru/internship/cloud/discovery/EurekaServerApplication.java
+++ src/main/java/ru/internship/cloud/discovery/EurekaServerApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication
 public class EurekaServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
